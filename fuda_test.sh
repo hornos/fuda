d6716340532f5119a92402b03582a96b37a20b6b
Tom Hornos <tom.hornos@gmail.com>
2012-06-25 22:46:22 +0200

    fix

--- fuda_test.sh
+++ fuda_test.sh
@@ -3,12 +3,15 @@
 gbn=$(basename ${BASH_SOURCE})
 gbn=${gbn%%.sh}
 
+_cuda_home=${CUDA_HOME:-/opt/cuda}
+_arch=$(uname -m | awk -F_ '{print $2}')
+
 if test "${OSTYPE##darwin}" = "${OSTYPE}" ; then
   # linux
-  LD_LIBRARY_PATH="${CUDA_HOME:-/opt/cuda}/lib"
+  LD_LIBRARY_PATH="${_cuda_home}/lib${_arch}"
 else
   # osx
-  DYLD_LIBRARY_PATH="${CUDA_HOME:-/usr/local/cuda}/lib"
+  DYLD_LIBRARY_PATH="${_cuda_home}/lib"
 fi
 
 ./${gbn} $*
